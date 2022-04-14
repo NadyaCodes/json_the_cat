b3f330d75188c3b451096be36aca2263e82b4091
NadyaCodes <nadyacorscadden@gmail.com>
2022-04-14 17:58:39 -0400

    made a test and sorted out a callback function that actually worked properly

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,24 +1,25 @@
 const request = require('request');
 
-const args = process.argv.slice(2);
 
-let where = `https://api.thecatapi.com/v1/breeds/search?q=${args[0]}`;
+const fetchBreedDescription = function(breedName, callback) {
 
+  let where = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
-request(`${where}`, (error, response, body) => {
+  request(`${where}`, (error, response, body) => {
 
-  if (error) {
-    console.error(error);
-    return;
-  }
-  const data = JSON.parse(body);
+    const data = JSON.parse(body);
 
-  if (!data[0]) {
+    const printDesc = (siteDesc) => {
+      if (!siteDesc[0]) {
+        return "Try again: that breed doesn't exist";
+      }
+      return siteDesc[0].description;
+    };
 
-    console.log("Try again: that breed doesn't exist");
-    return;
-  }
+    callback(error, printDesc(data));
 
+  });
 
-  console.log(data[0].description);
-});
+};
+
+module.exports = { fetchBreedDescription };
